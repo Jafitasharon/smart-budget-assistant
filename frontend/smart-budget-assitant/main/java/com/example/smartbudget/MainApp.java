84c464787941b686c66c156a70edb5c18af1601c
Jafita <jafitsharon4@gmail.com>
2025-08-17 13:14:20 +0530

    Add main Java controllers and FXML screens

---
+++ frontend/smart-budget-assitant/main/java/com/example/smartbudget/MainApp.java
@@ -0,0 +1,5 @@
+package frontend.smart-budget-assitant.main.java.com.example.smartbudget;
+
+public class MainApp {
+    
+}
---
+++ frontend/smart-budget-assitant/main/java/com/example/smartbudget/controller/BudgetController.java
@@ -0,0 +1,5 @@
+package frontend.smart-budget-assitant.main.java.com.example.smartbudget.controller;
+
+public class BudgetController {
+    
+}
